8f55b27fb31d6dcd9baf31071e32aa2185039ff6
WagnerFFreitas <wagnerffreitas1973@gmail.com>
2024-01-12 19:21:21 -0300

    Atualização da ortografia e do codigo

--- Exercicio03_Aula06/operadoresaritmeticos06.js
+++ Exercicio03_Aula06/operadoresaritmeticos06.js
@@ -1,5 +1,5 @@
 let valor = 123//numero
 let valor1 = "123"//Entre aspas se torna um texto
-console.log(valor + 5);//Nesta situação e somado o valor da variável VALOR +5
-console.log(valor1 + 5);//Neste situação e somado ao texto o numero 5
+console.log(valor + 5);//Nesta situação é somado o valor da variável VALOR +5
+console.log(valor1 + 5);//Neste situação é somado ao texto o numero 5
 console.log("123" + 5);//Tera o mesmo 

35200ea44f3145ca2a5d1caa2e8cddb82f1ee728
WagnerFFreitas <wagnerffreitas1973@gmail.com>
2024-01-25 10:36:45 -0300

    Atualização

--- Exercicio05_Aula08/operadoresdecomparacao.js
+++ Exercicio05_Aula08/operadoresdecomparacao.js
@@ -5,18 +5,19 @@ let vazia;
 let verdade = numero > 100; //Verifica se a variável e verdadeira (TRUE)
 let inverdade = numero <10; //Verifica se a variável e falsa (FALSE) 
 
-console.log("Numero: ",numero);
-console.log("Texto: ",texto);
-console.log("Vazia: ",vazia);
-console.log("Verdade: ",verdade);
+//1ª forma de checa o tipo de uma variável
+console.log("Numero: ",numero);//Se caso quiser exibir o numero
+console.log("Texto: ",texto);//Se caso quiser exibir o texto
+console.log("Vazia: ",vazia);// Vai exibir a variável vazia
+console.log("Verdade: ",verdade);//Se caso quiser exibir verdadeiro ou falso
 
-//Checa o tipo de uma variável
+//2ª forma de checa o tipo de uma variável
 console.log("Tipo da variável numero: ", typeof numero);
 console.log("Tipo da variável texto: ", typeof texto);
 console.log("Tipo da variável vazia: ", typeof vazia);
 console.log("Tipo da variável verdade: ", typeof verdade);
 
-//A linha abaixo e so para separar o conteudo de cima
+//A linha abaixo e so para separar o conteúdo de cima
 //uma forma simples
 console.log("--------------------------------------")
 
@@ -27,9 +28,13 @@ console.log("É maior ou igual?", numero >= 5);
 console.log("É menor?", numero < 10000);
 console.log("É menor ou maior?", numero <= 5436);
 
-//A linha abaixo e so para separar o conteudo de cima
+//A linha abaixo e so para separar o conteúdo de cima
 //uma forma simples
 console.log("--------------------------------------")
 
+//Aqui sera feito uma uma comparação
 console.log("É igual?", numero === 900);
+console.log("É igual?", texto === 900);
+console.log("É igual?", texto === "oi");
 console.log("É diferente?", texto !== "oi");
+console.log("É diferente?", texto !== "Ola");

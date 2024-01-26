75df7564e3ea3342b534e31fae12d0084091d0e5
WagnerFFreitas <wagnerffreitas1973@gmail.com>
2024-01-26 12:24:03 -0300

    Atualização

--- Exercicio07_Aula10_12/exemploscomcondicionais02.js
+++ Exercicio07_Aula10_12/exemploscomcondicionais02.js
@@ -1,24 +1,24 @@
-let precoDoProduto01 = 19.29;
-let precoDoProduto02 = 0.80;
-let precoDoProduto03 = 4.85;
+let precodoproduto01 = 19.29;
+let precodoproduto02 = 0.80;
+let precodoproduto03 = 4.85;
 
 let custo = 0;//Variavel criada para calcula a gasto com os produtos
 
 //Se o produto 01 for menor que R$18,00 comprar, nao foi incluso a condicional.
-if (precoDoProduto01 <= 18){
+if (precodoproduto01 <= 18){
     console.log("Comprei o produto 01");
-    //Na linha 9 e modificado o valor da variavel
-    custo = custo + precoDoProduto01;
+    //Na linha 9 e modificado o valor da variável
+    custo = custo + precodoproduto01;
 }
 //Se o produto 02 for menor que R$3,00 e maior ou igual a R$1.5 comprar,
 // e se for menor que R$1.5 informar que algo esta errado.
-if (precoDoProduto02 < 3){
-    if (precoDoProduto02 => 1.5){
+if (precodoproduto02 < 3){
+    if (precodoproduto02 => 1.5){
         console.log("Comprei o produto 02");
         //pode ser usado duas formas de calculo
-        custo += precoDoProduto02;
-        //Pode ser usado desta forma tambem
-        //custo = custo + precoDoProduto02;
+        custo += precodoproduto02;
+        //Pode ser usado desta forma também
+        //custo = custo + precodoproduto02;
     }else{
         console.log("Algo esta errado com o produto 02");
     }

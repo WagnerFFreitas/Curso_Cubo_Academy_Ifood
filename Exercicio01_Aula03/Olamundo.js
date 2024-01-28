f4d76042bedd1feb8d6c6970652a508eb875e702
WagnerFFreitas <wagnerffreitas1973@gmail.com>
2024-01-28 11:08:50 -0300

    Atualização

--- Exercicio01_Aula03/Olamundo.js
+++ Exercicio01_Aula03/Olamundo.js
@@ -1,7 +1,7 @@
 //Obs:
 //Sempre ficar atento a letras maiúsculas e minusculas.
 //Sempre finalizar uma linha de comando com ponto e virgula exemplo : console.log("Ola mundo !");
-//Comando CONSOLE,LOG exibe mensagem
+//Comando CONSOLE.LOG exibe mensagem
 console.log("Ola mundo !");
 console.log("Como você esta?");
 

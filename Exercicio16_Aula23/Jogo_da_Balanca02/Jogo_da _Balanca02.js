d9d731657dc7e948a950c4e4fbf408b6ce001912
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-05 22:52:22 -0300

    Atualizar o Jogo_da _Balanca02.js

--- Exercicio16_Aula23/Jogo_da_Balanca02/Jogo_da _Balanca02.js
+++ Exercicio16_Aula23/Jogo_da_Balanca02/Jogo_da _Balanca02.js
@@ -1,33 +1,35 @@
+
+
+
 // pesoladoB2 = 18
 // Se a soma do lado A soma do lado B -> inclinado para o lado A
 // Se não, soma do lado B > soma do lado A -> inclinado para o lado
 // Caso contrário -> equilíbrio
-const pesoLadoA1 = 10;
-const pesoLadoA2 = 5;
-const pesoLadoB1 = 20;
-const pesoLadoB2 = 10;I
-const somaLadoA = pesoLadoA1 + pesoLadoA2;
-const somaLadoB = pesoLadoB1+ pesoLadoB2;
-if (somaLadoA > somaLadoB) {
+
+const pesoladoA1 = 10;
+const pesoladoA2 = 5;
+const pesoladoB1 = 20;
+const pesoladoB2 = 10;
+const somaladoA = pesoladoA1 + pesoladoA2;
+const somaladoB = pesoladoB1+ pesoladoB2;
+if (somaladoA > somaladoB) {
     console.log("LADO A")
-    } else if (somaLadoB > somaLadoA) {
+    } else if (somaladoB > somaladoA) {
     console.log("LADO B")
     } else {
     console.log("EQUILÍBRIO")
 }
 
-function solucao (pesoLadoA1, peso LadoA2, peso LadoB1, pesoLadoB2) {
-const pesoLadoA1 = 10
-const pesoLadoA2 = 10
+function solucao(pesoladoA1, pesoladoA2, pesoladoB1, pesoladoB2) {
+  const somaladoA = pesoladoA1 + pesoladoA2;
+  const somaladoB = pesoladoB1 + pesoladoB2;
+
+  if (somaladoA > somaladoB) {
+    console.log("LADO A");
+  } else if (somaladoB > somaladoA) {
+    console.log("LADO B");
+  } else {
+    console.log("EQUILIBRIO");
+  }
 }
-const peso LadoB1 = 10 I
-const peso LadoB2 = 10
-const soma LadoA = pesoLadoA1 + pesoLadoA2
-const soma LadoB = pesoLadoB1 + pesoLadoB2
-if (soma LadoA > somaLadoB) {
-console.log("LADO A")
-} else if (soma LadoB > somaLadoA) {
-console.log("LADO B")
-} else {
-console.log("EQUILIBRIO")
-}
+

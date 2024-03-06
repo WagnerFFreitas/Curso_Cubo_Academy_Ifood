cabff4957e5f143838a307b5120d0389f601b70b
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-06 19:18:37 -0300

    Atualizar o Jogo_da_Balanca02.js

--- Exercicio16_Aula23/Exercicos/Jogo_da_Balanca02.js
+++ Exercicio16_Aula23/Exercicos/Jogo_da_Balanca02.js
@@ -14,18 +14,22 @@
 // Caso contrário -> equilíbrio
 
 //Neste exemplo como as variáveis nao sao alteradas no decorrer do código foi utilizado o CONST
+
+// Neste bloco abaixo e definido os pesos para cada lado
 const pesoladoA1 = 10;
 const pesoladoA2 = 5;
 const pesoladoB1 = 20;
 const pesoladoB2 = 10;
+
+// O bloco abaixo calcula a soma dos pesos para cada lado
 const somaladoA = pesoladoA1 + pesoladoA2;
-const somaladoB = pesoladoB1+ pesoladoB2;
+const somaladoB = pesoladoB1 + pesoladoB2;
 
+// O bloco abaixo compara os pesos e determinando o resultado
 if (somaladoA > somaladoB) {
-    console.log("LADO A")
-    } else if (somaladoB > somaladoA) {
-    console.log("LADO B")
-    } else {
-    console.log("EQUILÍBRIO")
+    console.log("LADO A");
+} else if (somaladoB > somaladoA) {
+    console.log("LADO B");
+} else {
+    console.log("EQUILÍBRIO");
 }
-

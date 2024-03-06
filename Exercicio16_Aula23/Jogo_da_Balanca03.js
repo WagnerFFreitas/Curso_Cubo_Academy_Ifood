77820da400abc19003dc971ab199ec9dc07b4f44
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-05 23:33:23 -0300

    Atualizar o Jogo_da_Balanca03.js

--- Exercicio16_Aula23/Jogo_da_Balanca03.js
+++ Exercicio16_Aula23/Jogo_da_Balanca03.js
@@ -1,8 +1,3 @@
-// pesoLadoA1= 10
-// pesoLadoA2= 5
-// pesoLadoB1= 20
-
-
 //Logica do jogo da balança
 
 // pesoLadoA1 = 10
@@ -14,23 +9,26 @@
 // Se não, soma do lado B > soma do lado A -> inclinado para o lado
 // Caso contrário -> equilíbrio
 
-// Função que compara pesos em dois lados (A e B)
+// A linha abaixo cria uma função que compara pesos em dois lados (A e B)
 function solucao(pesoladoA1, pesoladoA2, pesoladoB1, pesoladoB2) {
-  // Calcula a soma dos pesos do lado A
+  
+// A linha abaixo calcula a soma dos pesos do lado A
   const somaladoA = pesoladoA1 + pesoladoA2;
 
-  // Calcula a soma dos pesos do lado B
+  // A linha abaixo calcula a soma dos pesos do lado B
   const somaladoB = pesoladoB1 + pesoladoB2;
 
-  // Verifica se o lado A tem um peso maior que o lado B
+  // A linha abaixo verifica se o lado A tem um peso maior que o lado B
   if (somaladoA > somaladoB) {
     console.log("LADO A");
   } 
-  // Se o lado B tem um peso maior que o lado A
+  
+  // A linha abaixo informa se o lado B tem um peso maior que o lado A
   else if (somaladoB > somaladoA) {
     console.log("LADO B");
-  } 
-  // Se os pesos em ambos os lados são iguais
+  }
+ 
+  // A linha abaixo se os pesos em ambos os lados são iguais
   else {
     console.log("EQUILIBRIO");
   }

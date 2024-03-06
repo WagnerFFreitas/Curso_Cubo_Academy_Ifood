002077d0a8f1af111e1f8124de45929fce9d54c4
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-05 22:55:58 -0300

    Atualizar o Jogo_da _Balanca02.js

--- Exercicio16_Aula23/Jogo_da_Balanca02/Jogo_da _Balanca02.js
+++ Exercicio16_Aula23/Jogo_da_Balanca02/Jogo_da _Balanca02.js
@@ -1,4 +1,12 @@
+//Logica do Jogo da balança
+// pesoLadoA1= 10
+// pesoLadoA2= 5
+// pesoLadoB1= 20
+// pesoLadoB2 = 10
 
+// Se a soma do lado A soma do lado B -> inclinado para o lado A
+// Se não, soma do lado B > soma do lado A -> inclinado para o lado
+// Caso contrário -> equilíbrio
 
 
 // pesoladoB2 = 18
@@ -12,24 +20,12 @@ const pesoladoB1 = 20;
 const pesoladoB2 = 10;
 const somaladoA = pesoladoA1 + pesoladoA2;
 const somaladoB = pesoladoB1+ pesoladoB2;
+
 if (somaladoA > somaladoB) {
     console.log("LADO A")
     } else if (somaladoB > somaladoA) {
     console.log("LADO B")
     } else {
     console.log("EQUILÍBRIO")
-}
-
-function solucao(pesoladoA1, pesoladoA2, pesoladoB1, pesoladoB2) {
-  const somaladoA = pesoladoA1 + pesoladoA2;
-  const somaladoB = pesoladoB1 + pesoladoB2;
-
-  if (somaladoA > somaladoB) {
-    console.log("LADO A");
-  } else if (somaladoB > somaladoA) {
-    console.log("LADO B");
-  } else {
-    console.log("EQUILIBRIO");
-  }
 }
 

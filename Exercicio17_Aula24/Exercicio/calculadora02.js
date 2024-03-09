4d33d838d36384613ec031f7112a9bbb9523c40c
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-09 11:26:28 -0300

    Atualizar o calculadora02.js

--- Exercicio17_Aula24/Exercicio/calculadora02.js
+++ Exercicio17_Aula24/Exercicio/calculadora02.js
@@ -6,17 +6,18 @@
 //Se não, verificar se é uma multiplicacao -> numero1 x numero2
 //Se não, verificar se é uma divisao -> numero1 / numero2
 
+//O bloco abaixo cria as variáveis
 let operacao = "subtracao"
 let numero1 = 10
 let numero2 = 5
 
-
+// O bloco abaixo verifica o tipo de operação
 if (operacao === "soma") {
-  console.log(numero1 + numero2)
+  console.log(numero1 + numero2);  // Se a operação for soma, exibe a soma dos números
 } else if (operacao === "subtracao") {
-  console.log(numero1- numero2)
+  console.log(numero1 - numero2);  // Se a operação for subtração, exibe a subtração dos números
 } else if (operacao === "multiplicacao") {
-  console.log(numero1* numero2)
+  console.log(numero1 * numero2);  // Se a operação for multiplicação, exibe o produto dos números
 } else if (operacao === "divisao") {
-  console.log(numero1/ numero2)
+  console.log(numero1 / numero2);  // Se a operação for divisão, exibe o resultado da divisão dos números
 }

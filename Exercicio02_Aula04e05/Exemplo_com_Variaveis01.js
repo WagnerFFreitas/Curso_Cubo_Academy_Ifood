de72371ff4f9d1717489e493ce550b9d610dc7f5
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-11 20:17:19 -0300

    Atualizar o Exemplo_com_Variaveis01.js

--- Exercicio02_Aula04e05/Exemplo_com_Variaveis01.js
+++ Exercicio02_Aula04e05/Exemplo_com_Variaveis01.js
@@ -1,7 +1,9 @@
 const endereco = "Avenida 1"; // O comando CONST o seu valor após ser fixado não pode ser alterado no decorrer do código. 
 
+// O bloco abaixo cria as variáveis.
 let precodoleite = 9.5; // O ponto representa a virgula na separação dos números (casas decimais)
 let precodoqueijo = 30; // Toda variável a ser declarada não pode ter espaço.
 
-console.log ("Preço do leite: ", precodoleite);
-console.log ("Preço do queijo: ", precodoqueijo);
+
+console.log ("Preço do leite: ", precodoleite); // Exibe o preço do leite.
+console.log ("Preço do queijo: ", precodoqueijo); // Exibe o preço do queijo

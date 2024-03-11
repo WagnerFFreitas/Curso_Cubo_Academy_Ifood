c2b9b8898b7c115ac055fa3cf1e1f6e8aa7d563f
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-11 20:08:59 -0300

    Atualizar o operadoreslogicoe04.js

--- Exercicio09_Aula13_14/operadoreslogicoe04.js
+++ Exercicio09_Aula13_14/operadoreslogicoe04.js
@@ -1,9 +1,11 @@
-//Menores de 18 --> Menor de idade (meia)
-//Entre 18 e 60 --> Adultos (meio apenas com carteirinha)
-//Maiores de 60 anos --> Idoso (meia)
-//Meia --> O resto
-//Inteira --> Adulto e não tem carteirinha
+// Logica do programa.
+// Menores de 18 --> Menor de idade (meia)
+// Entre 18 e 60 --> Adultos (meio apenas com carteirinha)
+// Maiores de 60 anos --> Idoso (meia)
+// Meia --> O resto
+// Inteira --> Adulto e não tem carteirinha
 
+// Bloco abaixo cria as variáveis.
 const idade = 75;
 const temcarterinha = true;
 const ehadulta = idade >= 18 && idade <= 60; // Idade maior ou igual (&& e mesmo que e, sendo ele um operador logico) idade menor ou igual a 60
@@ -13,5 +15,5 @@ if (idade < 18 || idade > 60 || (ehAdulta && temcarterinha)){
 // So vai entrar no else se todas as condições acima for falsas
     console.log ("Meia");
 } else {
-    console.log("Inteira");
+    console.log ("Inteira");
 } 

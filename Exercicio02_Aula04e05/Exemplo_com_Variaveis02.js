c790f7147d2d19fb38498f5395fe90884179e23d
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-11 20:21:26 -0300

    Atualizar o Exemplo_com_Variaveis02.js

--- Exercicio02_Aula04e05/Exemplo_com_Variaveis02.js
+++ Exercicio02_Aula04e05/Exemplo_com_Variaveis02.js
@@ -1,13 +1,15 @@
 const endereco="Avenida 1"; // CONST : O seu valor após ser fixado nao pode ser alterado no decorrer do programa. 
 
+// O bloco abaixo cria as variáveis.
 let precodoleite = 9.5;
 let precodoqueijo = 30;
 // LET : O seu valor pode ser alterado de acordo coma a necessidade.
 
-console.log ("Preço do leite: ", precodoleite);
-console.log ("Preço do queijo: ", precodoqueijo);
-precodoleite = 8;
+console.log ("Preço do leite: ", precodoleite); // Exibe o preço do leite.
+console.log ("Preço do queijo: ", precodoqueijo); // Exibe o preço do queijo.
+
+precodoleite = 8; // Novo preço do leite.
 
 // O valor da variável precodoleite não e exibido até que as linhas abaixo seja incluídas para puxar os valores.
-console.log ("Preço do leite: ", precodoleite);
-console.log ("Preço do queijo: ", precodoqueijo);
+console.log ("Preço do leite: ", precodoleite); // Exibe o preço do leite.
+console.log ("Preço do queijo: ", precodoqueijo); // Exibe o preço do queijo.

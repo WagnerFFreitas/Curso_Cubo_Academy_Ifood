3cdfaaf9ecba928f569c30aaa5ea68a213231107
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-11 23:32:02 -0300

    Atualizar o Operadores_Aritmeticos02.js

--- Exercicio03_Aula06/Operadores_Aritmeticos02.js
+++ Exercicio03_Aula06/Operadores_Aritmeticos02.js
@@ -1,5 +1,6 @@
 let valor = 10; // Esta linha cria uma variável com o nome VALOR.
 
+// O bloco abaixo exibirá o valor assim que for calculado as operações com o valor da variável.
 console.log (valor + 9);
 console.log (valor - 9);
 console.log (valor * 9);

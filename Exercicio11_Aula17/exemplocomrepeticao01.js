41a1293f3ac3338b3558940ced1e20b5a75b1199
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-11 23:48:14 -0300

    Atualizar o exemplocomrepeticao01.js

--- Exercicio11_Aula17/exemplocomrepeticao01.js
+++ Exercicio11_Aula17/exemplocomrepeticao01.js
@@ -1,12 +1,17 @@
-//Calcula o rendimento de uma conta
+// Logica do codigo
+// Calcula o rendimento de uma conta
+// Todo ano ela rende 10%
+// Todo mês fazer um deposito
+// Quantos meses demora para chegar a 100.000
+
+// Bloco abaixo cria as variáveis.
 const deposito = 500;
 const objetivo = 100000;
 const rendimentoanual = 0.1;
 let conta=0;
 let meses=0;
-//Todo ano ela rende 10%
-//Todo mês fazer um deposito
-//Quantos meses demora para chegar a 100.000
+
+
 while (conta<objetivo){
     conta+=deposito;
     conta+=conta*rendimentoanual/12;

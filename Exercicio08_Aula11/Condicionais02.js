ece21b16e36f999f7a412d87769349130f3ee946
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-11 23:37:16 -0300

    Atualizar o Condicionais02.js

--- Exercicio08_Aula11/Condicionais02.js
+++ Exercicio08_Aula11/Condicionais02.js
@@ -2,7 +2,7 @@ const condicao = 3 < 2;
 
 // Sendo verdadeiro a variável entra no IF.
 if (condicao) {
-    console.log("é verdade");
+    console.log ("é verdade");
 
 // Sendo falso a variável entra no ELSE
 } else {

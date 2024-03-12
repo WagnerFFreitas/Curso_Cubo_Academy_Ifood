0d766315b391ed7bbcee10a1b2d4498f25a3da08
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-11 23:28:52 -0300

    Atualizar o Valores_e_Variaveis02.js

--- Exercicio02_Aula04e05/Valores_e_Variaveis02.js
+++ Exercicio02_Aula04e05/Valores_e_Variaveis02.js
@@ -1,5 +1,6 @@
+// O bloco abaixo cria as variáveis.
 let nome = "Wagner";
 const idade = 50; // O comando CONST não permite que a variável seja alterada no decorrer do código
 
-console.log (nome);
-console.log (idade);
+console.log (nome); // Exibe o nome.
+console.log (idade); // Exibe a idade.

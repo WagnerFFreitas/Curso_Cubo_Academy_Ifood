80f84fb87a4084e2481d3fd17534d9cb0e0c41c3
WagnerFFreitas <wagnerffreitas1973@gmail.com>
2024-03-12 20:22:51 -0300

    Atualização

--- Exercicio15_Aula21/Exemplo_de_Lista.js
+++ Exercicio15_Aula21/Exemplo_de_Lista.js
@@ -1,29 +1,25 @@
-const listadenumeros = [34, 14, 56, 768, 8, 81, 3, 6, 10];
-// 1. Qual a soma deles?
-//A contagem ira começar a parir do valor 0
-//na variável ACUMULADOR 
-let acumulador = 0;
+const listadenumeros = [34, 14, 56, 768, 8, 81, 3, 6, 10]; // 1. Qual a soma deles?
+
+let acumulador = 0; // Lembrando que a contagem ira começar a parir do valor 0 na variável ACUMULADOR.
 for (const numero of listadenuumeros) {|
   acumulador += numero;
 }
-//Será exibido o valor da soma dos números na
-//variável LISTADENUMEROS
-console.log("A soma é", acumulador);
+
+console.log ("A soma é", acumulador); // Será exibido o valor da soma dos números na variável LISTADENUMEROS.
 
 // 2. Qual o menor deles?
-//A variável OMENORNUMEROATEAGORA terá o valor fazio
-// UNDERFINED
 
-let omenornumeroateagora;
+let omenornumeroateagora; // A variável OMENORNUMEROATEAGORA terá o valor fazio UNDERFINED.
+
 for (const numero of listadenumeros) {
   if (omenornumeroateagora === undefined) {
     omenornumeroateagora = numero;
-// Se o numero for menor que o próximo 
-// então parar e exibir qual é o numero
-} else {
-  if (numero < omenornumeroateagora) {
-    omenornumeroateagora = numero;
+
+  // Se o número for menor que o próximo então para e exibir qual é o numero.
+  } else {
+    if (numero < omenornumeroateagora) {
+      omenornumeroateagora = numero;
+    }
   }
 }
-}
-console.log("O menor número é: ", omenornumeroateagora);
+console.log ("O menor número é: ", omenornumeroateagora);
--- Exercicio15_Aula21/README.md
+++ Exercicio15_Aula21/README.md
@@ -1,4 +1,4 @@
 # Exercicio16_Aula21
 Aula 21_Logica de programação da IFood_Cubo Academy
 
-Este exemplo mostra de forma simplificada como pode ser usado lista (Array)
+***Este exemplo mostra de forma simplificada como pode ser usado lista (Array)***

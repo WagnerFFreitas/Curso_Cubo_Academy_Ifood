8d1c2d60bc563913ac0d166eff98eabfe1453e6f
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-11 23:27:07 -0300

    Atualizar o Valores_e_Variaveis01.js

--- Exercicio02_Aula04e05/Valores_e_Variaveis01.js
+++ Exercicio02_Aula04e05/Valores_e_Variaveis01.js
@@ -1,4 +1,6 @@
+// O bloco abaixo cria as variáveis.
 let nome = "Wagner";
 let idade = 50;
-console.log (nome);
-console.log (idade);
+
+console.log (nome); // Exibe o nome.
+console.log (idade); // Exibe a idade.

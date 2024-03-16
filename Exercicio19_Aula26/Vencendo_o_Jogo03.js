559c185d68c1ffc56df2b73d121338df286c0c88
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-15 22:37:39 -0300

    Atualizar o Exercicio19_Aula26

---
+++ Exercicio19_Aula26/Vencendo_o_Jogo03.js
@@ -0,0 +1,14 @@
+function solucao(placar) {
+  let placar = [5, 8]
+
+  let quantidadegolstimeA =
+(parameter) placarany
+  let quantidadegolstimeB = placar [1]
+
+if (quantidadegolstimeA > quantidadegolstimeB) {
+  console.log("TIME A")
+} else if (quantidadegolstimeB > quantidadeholstimeA) {
+  console.log ("TIME B")
+} else {
+  console.log ("EMPATE")
+};

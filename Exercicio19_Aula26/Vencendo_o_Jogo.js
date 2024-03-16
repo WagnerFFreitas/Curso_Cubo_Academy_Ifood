2820fb8703542d44df233385a5021a4db575c4d1
Wagner Ferreira Freitas <96551845+WagnerFFreitas@users.noreply.github.com>
2024-03-15 22:22:06 -0300

    Atualizar o Vencendo_o_Jogo.js

---
+++ Exercicio19_Aula26/Vencendo_o_Jogo.js
@@ -0,0 +1,20 @@
+// Logica do código
+
+// placar = [3, 2]
+
+// Se a quantidade de gols do Time A for maior que a do Time B
+// -> TIME A
+
+//Se não, se a quantidade de gols do Time B é maior que a do TimeA
+// -> TIME B
+
+// Caso contrário -> EMPATE
+
+let placar [5, 3];
+let quantidadegolstimeA =
+if (quantidadegolstimeA > quantidadegolstimeB) {
+  console.log("TIME A")
+} else if (quantidadegolstimeB > quantidadegolstimeA) {
+  console.log ("TIME B")
+} else {
+console.log ("EMPATE");

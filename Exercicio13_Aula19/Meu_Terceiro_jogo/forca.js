59f2337070094a9c6ec854b5c28bfa6a4ab24f51
Wagner Ferreira Freitas <wagnerffreitas1973@gmail.com>
2024-06-24 19:48:45 -0300

    Criação dos olhos e termino da perna direita,

--- Exercicio13_Aula19/Meu_Terceiro_jogo/forca.js
+++ Exercicio13_Aula19/Meu_Terceiro_jogo/forca.js
@@ -57,7 +57,7 @@ function exibirenforcado() {
   var pessoaEnforcadaElement = document.getElementById('pessoa-enforcada');
   var partesAdicionais = {
     "braco-esquerdo": ["braco-esquerdo-dedo1", "braco-esquerdo-dedo2", "braco-esquerdo-dedo3", "braco-esquerdo-dedo4", "braco-esquerdo-dedo5"],
-    "braco-direito": ["braco-direito-dedo1", "braco-direito-dedo2", "braco-direito-dedo3", "braco-direito-dedo4", "braco-direito-dedo5"], "perna-esquerda": ["perna-esquerda-sapato"],"perna-direita": ["perna-direito-sapato"]
+    "braco-direito": ["braco-direito-dedo1", "braco-direito-dedo2", "braco-direito-dedo3", "braco-direito-dedo4", "braco-direito-dedo5"], "perna-esquerda": ["perna-esquerda-sapato"],"perna-direita": ["perna-direito-sapato"], "cabeca": ["cabeca-olho-esquerdo", "cabeca-olho-direito", "orelha", "nariz", "cabelo", "boca"]
   };
  
   pessoaEnforcadaElement.innerHTML = "";
